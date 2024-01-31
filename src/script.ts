a8d8c2edbde622890469b9fb731fde6456ec9014
Krishna Khong <krishna@khong.dev>
2024-02-01 00:30:53 +0800

    feat: add book to table via event handlers

--- src/script.ts
+++ src/script.ts
@@ -1,4 +1,8 @@
+const myLibrary: Book[] = [];
+
 class Book {
+    private static id = 0;
+    private id: number;
     private author: string;
     private title: string;
     private numberOfPages: number;
@@ -10,10 +14,15 @@ class Book {
         numberOfPages: number,
         hasBeenRead: boolean
     ) {
+        this.id = ++Book.id;
         this.author = author;
         this.title = title;
         this.numberOfPages = numberOfPages;
         this.hasBeenRead = hasBeenRead;
+    }
+
+    public getId() {
+        return this.id;
     }
     
     public getTitle() {
@@ -42,12 +51,20 @@ class LibraryUI {
     private newBookBtn: HTMLButtonElement | null;
     private newBookDialog: HTMLDialogElement | null;
     private newBookDialogClose: HTMLButtonElement | null;
+    private newBookDialogSubmit: HTMLButtonElement | null;
 
-    constructor(containerId: string, newBookBtn: string, newBookDialog: string, newBookDialogClose: string) {
+    constructor(
+        containerId: string,
+        newBookBtn: string,
+        newBookDialog: string,
+        newBookDialogClose: string,
+        newBookDialogSubmit: string
+        ) {
         this.tableBody = document.getElementById(containerId) as HTMLTableElement;
         this.newBookBtn = document.getElementById(newBookBtn) as HTMLButtonElement;
         this.newBookDialog = document.getElementById(newBookDialog) as HTMLDialogElement;
         this.newBookDialogClose = document.getElementById(newBookDialogClose) as HTMLButtonElement;
+        this.newBookDialogSubmit = document.getElementById(newBookDialogSubmit) as HTMLButtonElement;
         
         this.newBookBtn?.addEventListener('click', () => {
             this.newBookDialog?.showModal();
@@ -57,41 +74,48 @@ class LibraryUI {
             e.preventDefault();
             this.newBookDialog?.close();
         });
-    }
-
-    public displayLibrary(library: Book[]) {
-        for (let i = 0; i < library.length; i++) {
-            const row = this.tableBody?.insertRow(-1);
-            
-            if (!row) {
+        
+        this.newBookDialogSubmit?.addEventListener('click', (e) => {
+            const form = document.getElementById('new-book-form') as HTMLFormElement;
+            if (!form.checkValidity()) {
                 return;
             }
-
-            const idCell = row?.insertCell(0);
-            const titleCell = row?.insertCell(1);
-            const authorCell = row?.insertCell(2);
-            const pagesCell = row?.insertCell(3);
-            const readCell = row?.insertCell(4);
-
-            idCell.textContent = `${i + 1}`;
-            titleCell.textContent = library[i].getTitle();
-            authorCell.textContent = library[i].getAuthor();
-            pagesCell.textContent = library[i].getNumberOfPages().toString();
-            readCell.textContent = library[i].getHasBeenRead() ? "Yes" : "No";
-        }
+            
+            e.preventDefault();
+            const author = document.getElementById('author') as HTMLInputElement;
+            const title = document.getElementById('title') as HTMLInputElement;
+            const pages = document.getElementById('pages') as HTMLInputElement;
+            const read = document.getElementById('read') as HTMLInputElement;
+            const newBook = new Book(author.value, title.value, parseInt(pages.value), read.checked);
+            Book.addToLibrary(newBook, myLibrary);
+            this.newBookDialog?.close();
+            this.displayLibrary(newBook);
+        });
     }
-    
-    public addBookToLibrary(book: Book) {
-        
+
+    public displayLibrary(book: Book) {
+        const row = this.tableBody?.insertRow();
+            
+        if (!row) {
+            return;
+        }
+
+        const idCell = row?.insertCell(0);
+        const titleCell = row?.insertCell(1);
+        const authorCell = row?.insertCell(2);
+        const pagesCell = row?.insertCell(3);
+        const readCell = row?.insertCell(4);
+
+        idCell.textContent = `${book.getId()}`;
+        titleCell.textContent = book.getTitle();
+        authorCell.textContent = book.getAuthor();
+        pagesCell.textContent = book.getNumberOfPages().toString();
+        readCell.textContent = book.getHasBeenRead() ? "Yes" : "No";
     }
 }
 
-const myLibrary: Book[] = [];
 const book1 = new Book("James Clear", "Atomic Habits", 300, false);
-const book2 = new Book("J.K. Rowling", "Harry Potter", 500, true);
 Book.addToLibrary(book1, myLibrary);
-Book.addToLibrary(book2, myLibrary);
 
-const myLibraryUI = new LibraryUI('library-body', 'new-book-btn', 'new-book-dialog', 'new-book-dialog-close');
-myLibraryUI.displayLibrary(myLibrary);
-
+const myLibraryUI = new LibraryUI('library-body', 'new-book-btn', 'new-book-dialog', 'new-book-dialog-close', 'new-book-submit');
+myLibraryUI.displayLibrary(book1);
